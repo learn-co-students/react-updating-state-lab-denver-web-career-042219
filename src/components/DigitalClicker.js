aae48da524d17d944703348b9568ffb13c1de9ea
ahare14 <alexanderhare14@gmail.com>
2019-10-15 16:31:13 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
 // Code DigitalClicker Component Here
+import React, { Component } from "react";
+
+export default class DigitalClicker extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState(prevState => {
+      prevState.timesClicked = prevState.timesClicked + 1
+      return prevState
+      
+    }, () =>  console.log(this.state.timesClicked))
+
+  }
+  
+  render () {
+    return (
+      <div>
+        <h1>This is the DigitalClicker Component</h1>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,49 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+  
+  constructor () {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBitrateClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    }, () => console.log(this.state.settings.bitrate))
+  } 
+  
+  handleResolutionClick = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          resolution: '720p'
+        }
+      }
+    }, () => console.log(this.state.settings.video.resolution))
+  }
+  
+  render () {
+    return (
+      <div>
+        <h1>This is the YouTube Debugger Component</h1>
+        <button className="bitrate" onClick={this.handleBitrateClick}>Bitrate Change</button>
+        <button className="resolution" onClick={this.handleResolutionClick}>Resolution Change</button>
+      </div>
+    )
+  }
+ }
